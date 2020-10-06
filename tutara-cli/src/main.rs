d520945cc0634af62d6b1f2fe6d5efa132c730e6
Marnix Wieffering <git@marnixwieffering.dev>
2020-10-06 15:01:21 +0200

    Add interactive mode
    Co-authored-by: git@ndat.nl <git@ndat.nl>

--- tutara-cli/src/main.rs
+++ tutara-cli/src/main.rs
@@ -3,14 +3,81 @@ extern crate tutara_interpreter;
 use std::env;
 use std::fs;
 use std::io;
+use std::io::Write;
 
 use tutara_interpreter::Tokenizer;
+
+fn run(src: String, print_input: bool) {
+	if print_input {
+		println!("# Input");
+		println!("{}", src);
+		println!();
+	}
+	
+	let tokenizer = Tokenizer::new(&src);
+
+	println!("# Tokens");
+	println!();
+	
+	let mut last_line = 1;
+	for result in tokenizer {
+		if let Err(err) = result {
+			println!("Error at line {} on column {}: {}", err.line, err.column, err.message);
+		} else if let Ok(token) = result {
+			let mut literal_val = "".to_string();
+	
+			if token.literal.is_some() {
+				literal_val =
+					String::new() + "literal=" + &token.literal.as_ref().unwrap().to_string() + " ";
+			}
+	
+			if token.line != last_line {
+				println!();
+				last_line = token.line;
+			}
+	
+			println!(
+				"{:<11}{:<30}line={:<4} column={:<4} length={:<4}",
+				format!("{:?}", token.r#type),
+				literal_val,
+				token.line,
+				token.column,
+				token.length
+			);
+		}
+	}
+}
+
+fn interactive_mode() {
+	println!("Initialized Tutara interactive mode. Enter \".exit\" to leave.");
+	println!();
+	
+	loop {
+		let mut input = String::new();
+		print!("> ");
+		io::stdout().flush().expect("Failed to write");
+		io::stdin().read_line(&mut input).expect("Failed to read");
+	
+		if input.starts_with(".exit") {
+			println!("Exiting interactive mode");
+			break;
+		} else {
+			run(input, false);
+		}
+	}
+}
+
+fn run_file(path: &String) {
+	println!("Reading from file {}", path);
+	
+	let source = fs::read_to_string(path).expect("Could not read file").to_string();
+
+	run(source, true);
+}
 
 fn main() {
 	// Read command line arguments
 	let args: Vec<String> = env::args().collect();
-
-	let source: String;
 
 	match args.get(1) {
 		// Read from argument
@@ -23,61 +90,15 @@ fn main() {
 				}
 
 				let file_path = file.unwrap();
-				println!("Reading from file {}", file_path);
-				source = fs::read_to_string(file_path)
-					.expect("Could not read file")
-					.to_string();
+				run_file(file_path);
 			} else {
 				println!("Reading from argument");
-				source = arg.to_string();
+				run(arg.to_string(), true);
 			}
 		}
 		// Read from stdin
 		None => {
-			println!("Reading from stdin");
-			let mut input = String::new();
-			print!("> ");
-
-			io::stdin().read_line(&mut input).expect("Failed to read");
-			source = input
+			interactive_mode();
 		}
 	};
-
-	println!("# Input");
-	println!("{}", source);
-	println!();
-
-	// Convert source to tokens
-	let tokenizer = Tokenizer::new(&source);
-
-	println!("# Tokens");
-	println!();
-
-	let mut last_line = 1;
-	for result in tokenizer {
-		if let Err(err) = result {
-			println!("Error at line {} on column {}: {}", err.line, err.column, err.message);
-		} else if let Ok(token) = result {
-			let mut literal_val = "".to_string();
-
-			if token.literal.is_some() {
-				literal_val =
-					String::new() + "literal=" + &token.literal.as_ref().unwrap().to_string() + " ";
-			}
-
-			if token.line != last_line {
-				println!();
-				last_line = token.line;
-			}
-
-			println!(
-				"{:<11}{:<30}line={:<4} column={:<4} length={:<4}",
-				format!("{:?}", token.r#type),
-				literal_val,
-				token.line,
-				token.column,
-				token.length
-			);
-		}
-	}
 }
