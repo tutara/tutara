0223087c391416544d9689d8195538dd30505c2c
Niels van Velzen <git@ndat.nl>
2020-11-02 11:51:09 +0100

    Rewrite wasm library

--- tutara-wasm/src/lib.rs
+++ tutara-wasm/src/lib.rs
@@ -1,76 +1,54 @@
 use wasm_bindgen::prelude::*;
 
-use tutara_interpreter::Literal;
 use tutara_interpreter::Token;
-use tutara_interpreter::TokenType;
 use tutara_interpreter::Tokenizer;
+use tutara_interpreter::{Parser, Statement};
 
 #[wasm_bindgen]
-pub struct LocalToken {
-	token_type: TokenType,
-	literal: Option<Literal>,
-	pub line: u32,
-	pub column: u32,
-	pub length: u32,
-}
-
-impl LocalToken {
-	pub fn from_token(token: Token) -> LocalToken {
-		LocalToken {
-			token_type: token.r#type,
-			literal: token.literal,
-			line: token.line,
-			column: token.column,
-			length: token.length,
-		}
-	}
+pub struct Source {
+    text: String,
+    tokens: Option<Vec<Token>>,
+    statements: Option<Vec<Statement>>,
 }
 
 #[wasm_bindgen]
-impl LocalToken {
-	#[wasm_bindgen(getter)]
-	pub fn token_type(&self) -> String {
-		self.token_type.to_string()
-	}
-	#[wasm_bindgen(getter)]
-	pub fn literal(&self) -> JsValue {
-		if let Some(literal) = &self.literal {
-			match literal {
-				Literal::Number(n) => JsValue::from_f64(*n as f64),
-				Literal::String(s) => JsValue::from_str(s),
-			}
-		} else {
-			JsValue::null()
-		}
-	}
-}
+impl Source {
+    #[wasm_bindgen]
+    pub fn get_tokens(&mut self) -> JsValue {
+        if self.tokens.is_none() {
+            let tokenizer = Tokenizer::new(&self.text);
+            self.tokens = Some(
+                tokenizer
+                    .filter(|token| token.is_ok())
+                    .map(|token| token.unwrap())
+                    .collect(),
+            );
+        }
 
-#[wasm_bindgen(module = "/token-set.js")]
-extern "C" {
-	pub type TokenSet;
+        JsValue::from_serde(&self.tokens).unwrap()
+    }
 
-	#[wasm_bindgen(constructor)]
-	fn new() -> TokenSet;
+    #[wasm_bindgen]
+    pub fn get_statements(&mut self) -> JsValue {
+        if self.statements.is_none() {
+            let parser = Parser::new(Tokenizer::new(&self.text).peekable());
+            self.statements = Some(
+                parser
+                    .filter(|statement| statement.is_ok())
+                    .map(|statement| statement.unwrap())
+                    .collect(),
+            );
+        }
 
-	#[wasm_bindgen(method)]
-	fn append(this: &TokenSet, token: LocalToken);
+        JsValue::from_serde(&self.statements).unwrap()
+    }
 }
 
 #[wasm_bindgen]
-pub fn get_tokens(source: &str) -> TokenSet {
-	let mut tokenizer = Tokenizer::new(source);
-	let token_set = TokenSet::new();
-
-	loop {
-		let result = tokenizer.next();
-
-		if result.is_none() {
-			break;
-		}
-
-		let token = result.unwrap();
-		token_set.append(LocalToken::from_token(token.unwrap()));
-	}
-
-	token_set
+pub fn from_source(source: &str) -> Source {
+    Source {
+        text: source.to_string(),
+        tokens: None,
+        statements: None,
+    }
 }
