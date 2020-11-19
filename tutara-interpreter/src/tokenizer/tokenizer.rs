f1ea9122b64e1b7307ee3c658061b3ec03864a30
Niels van Velzen <git@ndat.nl>
2020-11-19 11:58:51 +0100

    Fix length of assignment operator tokens

--- tutara-interpreter/src/tokenizer/tokenizer.rs
+++ tutara-interpreter/src/tokenizer/tokenizer.rs
@@ -249,7 +249,7 @@ impl Tokenizer<'_> {
 		if TokenType::is_operation(&r#type) {
 			if let Some(_next) = self.next_if_char('=') {
 				self.chars.next();
-				self.length += 1;
+				self.length += 2;
 				return match r#type {
 					TokenType::Plus => self.create_token(TokenType::AssignPlus),
 					TokenType::Minus => self.create_token(TokenType::AssignMinus),
