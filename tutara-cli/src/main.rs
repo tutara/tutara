95995a40fa2445a5f87be31530c93abe331a516d
Niels van Velzen <git@ndat.nl>
2020-11-24 11:59:13 +0100

    Rewrite CLI

--- tutara-cli/src/main.rs
+++ tutara-cli/src/main.rs
@@ -1,14 +1,14 @@
 extern crate tutara_interpreter;
 
-use std::{env, fs};
+use std::env;
 use std::io;
-use std::io::Result;
-use std::io::Write;
-use tutara_interpreter::{Parser, Token, TokenType, Tokenizer};
+use std::io::{Read, Write};
+use std::result::Result;
 use tutara_compiler_llvm::Evaluator;
+use tutara_interpreter::{Parser, Statement, Token, TokenType, Tokenizer};
 
+use clap::{crate_version, App, AppSettings, Arg, ArgSettings};
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
-
 
 fn color_for_token(token: &Token) -> Option<Color> {
 	// Colors based on Nord color palette
@@ -63,8 +63,49 @@ fn color_for_token(token: &Token) -> Option<Color> {
 	}
 }
 
-fn run(src: String) -> Result<()> {
-	let mut stdout = StandardStream::stdout(ColorChoice::Auto);
+fn run(input: &str, output: &str, format: &str) -> Result<(), std::io::Error> {
+	let mut input_read: Box<dyn Read> = if input == "-" {
+		Box::new(std::io::stdin())
+	} else {
+		match std::fs::File::open(&input) {
+			Ok(file) => Box::new(file),
+			Err(_) => {
+				// file not found
+				unimplemented!();
+			}
+		}
+	};
+
+	let mut output_write: Box<dyn Write> = if output == "-" {
+		Box::new(std::io::stdout())
+	} else {
+		match std::fs::File::create(&output) {
+			Ok(file) => Box::new(file),
+			Err(_) => {
+				// file not found?
+				unimplemented!();
+			}
+		}
+	};
+
+	match format {
+		"highlight" => if output == "-" {
+			highlight(&mut input_read)
+		} else {
+			writeln!(output_write, "Highlight output can not be exported to files.")
+		},
+		"tokens" => tokenize(&mut input_read, &mut output_write),
+		"statements" => parse(&mut input_read, &mut output_write),
+		"result" => evaluate(&mut input_read, &mut output_write),
+		_ => unreachable!(),
+	}
+}
+
+fn highlight(input: &mut dyn std::io::Read) -> Result<(), std::io::Error> {	
+	let mut output = StandardStream::stdout(ColorChoice::Auto);
+	let mut src = String::new();
+	input.read_to_string(&mut src)?;
+
 	let tokenizer = Tokenizer::new(&src);
 	let mut line_index = 1;
 	let mut column_index = 0;
@@ -74,26 +115,26 @@ fn run(src: String) -> Result<()> {
 
 	for result in tokenizer {
 		if let Err(err) = result {
-			writeln!(&mut stdout, "{}", err)?;
+			writeln!(output, "{}", err)?;
 			break;
 		} else if let Ok(token) = result {
-			stdout.set_color(ColorSpec::new().set_fg(color_for_token(&token)))?;
+			output.set_color(ColorSpec::new().set_fg(color_for_token(&token)))?;
 
 			while token.line > line_index {
 				line_index += 1;
 				column_index = 0;
 				line = lines.next().unwrap();
-				writeln!(&mut stdout)?;
+				writeln!(output)?;
 			}
 
 			if column_index < token.column {
 				let diff = token.column - column_index;
-				write!(&mut stdout, "{}", " ".repeat(diff as usize))?;
+				write!(output, "{}", " ".repeat(diff as usize))?;
 				column_index += diff;
 			}
 
 			write!(
-				&mut stdout,
+				output,
 				"{}",
 				line.chars()
 					.skip(token.column as usize)
@@ -105,25 +146,54 @@ fn run(src: String) -> Result<()> {
 	}
 
 	// Reset colors
-	stdout.set_color(&ColorSpec::new())?;
-	writeln!(&mut stdout)?;
+	output.set_color(&ColorSpec::new())?;
 
-	// Evaluate
-	match Evaluator::evaluate(Parser::new(Tokenizer::new(&src).peekable())) {
-		Ok(output) => {
-			write!(&mut stdout, "Output: ")?;
-			writeln!(&mut stdout, "{}", output)?;
-		},
-		Err(err) => {
-			writeln!(&mut stdout, "{}", err)?;
-		}
-	};
-	
-	writeln!(&mut stdout)
+	writeln!(output)
 }
 
-fn interactive_mode() {
-	println!("Initialized Tutara interactive mode. Use \".file [path]\" to read files or \".exit\" to leave.");
+fn tokenize(input: &mut dyn std::io::Read, output: &mut dyn Write) -> Result<(), std::io::Error> {
+	let mut src = String::new();
+	input.read_to_string(&mut src)?;
+
+	let tokenizer = Tokenizer::new(&src);
+	let tokens: Result<Vec<Token>, tutara_interpreter::Error> = tokenizer.collect();
+
+	match tokens {
+		Ok(tokens) => writeln!(output, "{}", serde_json::to_string(&tokens).unwrap()),
+		Err(err) => writeln!(output, "Error: {}", err),
+	}
+}
+
+fn parse(input: &mut dyn std::io::Read, output: &mut dyn Write) -> Result<(), std::io::Error> {
+	let mut src = String::new();
+	input.read_to_string(&mut src)?;
+
+	let tokenizer = Tokenizer::new(&src);
+	let parser = Parser::new(tokenizer.peekable());
+	let statements: Result<Vec<Statement>, tutara_interpreter::Error> = parser.collect();
+
+	match statements {
+		Ok(statements) => writeln!(output, "{}", serde_json::to_string(&statements).unwrap()),
+		Err(err) => writeln!(output, "Error: {}", err),
+	}
+}
+
+fn evaluate(input: &mut dyn std::io::Read, output: &mut dyn Write) -> Result<(), std::io::Error> {
+	let mut src = String::new();
+	input.read_to_string(&mut src)?;
+
+	let tokenizer = Tokenizer::new(&src);
+	let parser = Parser::new(tokenizer.peekable());
+	let evaluation = Evaluator::evaluate(parser);
+
+	match evaluation {
+		Ok(evaluation) => writeln!(output, "{}", evaluation),
+		Err(err) => writeln!(output, "Error: {}", err),
+	}
+}
+
+fn interactive_mode() -> Result<(), std::io::Error> {
+	println!("Initialized Tutara interactive mode. Use \".exit\" to leave.");
 	println!();
 
 	let mut buffer = Vec::new();
@@ -136,56 +206,78 @@ fn interactive_mode() {
 		if input.starts_with(".exit") {
 			println!("Exiting interactive mode");
 			break;
-		} else if input.starts_with(".file") {
-			let parts = input.split_whitespace().nth(1);
-			if let Some(path) = parts {
-				run_file(&path.to_string());
-			} else {
-				println!("Invalid path. Syntax: .file [path]");
-			}
 		} else {
 			buffer.push(input.clone());
 
 			if input.starts_with("return") {
-				run(buffer.join("")).expect("Could not write the desired output");
+				evaluate(&mut buffer.join("").as_bytes(), &mut io::stdout())?;
 
 				buffer.clear();
 			}
 		}
 	}
+
+	Ok(())
 }
 
-fn run_file(path: &str) {
-	println!("Reading from file {}", path);
-	let source = fs::read_to_string(path).expect("Could not read file");
+fn main() -> Result<(), std::io::Error> {
+	let matches = App::new("Tutara")
+		.version(crate_version!())
+		.setting(AppSettings::ColoredHelp)
+		.setting(AppSettings::SubcommandRequiredElseHelp)
+		.setting(AppSettings::GlobalVersion)
+		.setting(AppSettings::HelpRequired)
+		.setting(AppSettings::VersionlessSubcommands)
+		.subcommand(
+			App::new("run")
+				.about("Run a script")
+				.arg(
+					Arg::new("input")
+						.short('i')
+						.about("Set input file or '-' to use STDIN")
+						.takes_value(true)
+						.required(true),
+				)
+				.arg(
+					Arg::new("output")
+						.short('o')
+						.about("Set output file or '-' to use STDOUT")
+						.takes_value(true)
+						.default_value("-"),
+				)
+				.arg(
+					Arg::new("format")
+						.short('f')
+						.about("Set format")
+						.setting(ArgSettings::Hidden)
+						.takes_value(true)
+						.possible_values(&["highlight", "tokens", "statements", "result"])
+						.default_value("result"),
+				),
+		)
+		.subcommand(
+			App::new("generate-test")
+				.about("Generate JSON files for a given test")
+				.setting(AppSettings::Hidden)
+				.arg(
+					Arg::new("NAME")
+						.about("Sets the name of the test")
+						.multiple(true)
+						.required(true),
+				),
+		)
+		.subcommand(App::new("interactive").about("Start interactive mode"))
+		.get_matches();
 
-	run(source).expect("Could not write the desired output");
-}
+	match matches.subcommand() {
+		Some(("run", run_matches)) => {
+			let input = run_matches.value_of("input").unwrap();
+			let output = run_matches.value_of("output").unwrap();
+			let format = run_matches.value_of("format").unwrap();
 
-fn main() {
-	// Read command line arguments
-	let args: Vec<String> = env::args().collect();
-
-	match args.get(1) {
-		// Read from argument
-		Some(arg) => {
-			if arg == "--file" {
-				let file = args.get(2);
-				if file.is_none() {
-					println!("ERROR: Please specify a file: --file [path]");
-					return;
-				}
-
-				let file_path = file.unwrap();
-				run_file(file_path);
-			} else {
-				println!("Reading from argument");
-				run(arg.to_string()).expect("Could not write the desired output");
-			}
+			run(input, output, format)
 		}
-		// Read from stdin
-		None => {
-			interactive_mode();
-		}
-	};
+		Some(("interactive", _)) => interactive_mode(),
+		_ => unreachable!(),
+	}
 }
