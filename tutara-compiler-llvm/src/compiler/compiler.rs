051500150d1755bdd2068ea605ab13eb5ed56dbb
Niels van Velzen <nielsvanvelzen@users.noreply.github.com>
2020-12-15 13:47:50 +0100

    Initial function support (#59)
    * Initial progress on function support
    
    * Review feedback

--- tutara-compiler-llvm/src/compiler/compiler.rs
+++ tutara-compiler-llvm/src/compiler/compiler.rs
@@ -3,13 +3,16 @@ use inkwell::{
 	builder::Builder,
 	context::Context,
 	module::Module,
+	types::BasicTypeEnum,
 	values::FloatValue,
 	values::InstructionValue,
-	values::{BasicValueEnum, FunctionValue, IntValue, PointerValue},
+	values::{BasicValue, BasicValueEnum, FunctionValue, IntValue, PointerValue},
 	FloatPredicate,
 };
 use std::collections::HashMap;
-use tutara_interpreter::{Parser, Analyzer, Error, Expression, Literal, Statement, Token, TokenType};
+use tutara_interpreter::{
+	Analyzer, Error, Expression, Literal, Parser, Statement, Token, TokenType,
+};
 
 pub struct Compiler<'a> {
 	pub(super) context: &'a Context,
@@ -24,6 +27,7 @@ pub struct Compiler<'a> {
 pub enum Scope<'a> {
 	While(BasicBlock<'a>, BasicBlock<'a>, BasicBlock<'a>), // Body , Evaluation , Continuation
 	If(BasicBlock<'a>, BasicBlock<'a>),                    // Body , Continuation
+	Fun,
 }
 
 pub enum Operation<'a> {
@@ -79,10 +83,111 @@ impl Compiler<'_> {
 			Body(statements) => self.evaluate_body(statements),
 			Return(expression) => self.evaluate_return(expression),
 			Comment(_) => Ok(Operation::NoOp),
+			Function(r#type, identifier, parameters, body) => {
+				self.evaluate_function(identifier, r#type, parameters, body)
+			}
 			_ => Err(Error::new_compiler_error(
 				"Unsupported statement".to_string(),
 			)),
 		}
+	}
+
+	pub fn evaluate_function(
+		&mut self,
+		identifier: Token,
+		r#type: Option<Token>,
+		parameters: Vec<(Token, Token)>,
+		body: Box<Statement>,
+	) -> Result<Operation, Error> {
+		// Get parameter types
+		let mut params: Vec<BasicTypeEnum> = Vec::new();
+
+		for parameter in parameters.iter() {
+			let r#type: BasicTypeEnum = match &parameter.1.literal {
+				Some(Literal::String(literal)) => match literal.as_str() {
+					"Int" => self.context.f64_type().into(),
+					"Bool" => self.context.bool_type().into(),
+					_ => {
+						return Err(Error::new_compiler_error(
+							"Invalid token/literal".to_string(),
+						))
+					}
+				},
+				_ => {
+					return Err(Error::new_compiler_error(
+						"Invalid token/literal".to_string(),
+					))
+				}
+			};
+
+			params.push(r#type)
+		}
+
+		// Get function return type
+		let fun_type = match r#type {
+			None => self.context.void_type().fn_type(&params, false),
+			Some(token) => match token.r#type {
+				TokenType::Identifier => match token.literal {
+					Some(Literal::String(literal)) => match literal.as_str() {
+						"Int" => self.context.f64_type().fn_type(&params, false),
+						"Bool" => self.context.bool_type().fn_type(&params, false),
+						_ => {
+							return Err(Error::new_compiler_error(
+								"Unknown return type".to_string(),
+							))
+						}
+					},
+					_ => return Err(Error::new_compiler_error("Unexpected literal".to_string())),
+				},
+				_ => return Err(Error::new_compiler_error("Unexpected token".to_string())),
+			},
+		};
+
+		// Get function name
+		let fun_name = match identifier.literal {
+			Some(Literal::String(str)) => str,
+			_ => {
+				return Err(Error::new_compiler_error(
+					"Invalid token/literal".to_string(),
+				))
+			}
+		};
+		
+		// Create function
+		let fun = self.module.add_function(fun_name.as_str(), fun_type, None);
+		let body_block = self
+			.context
+			.append_basic_block(fun, format!("{}_entry", fun_name).as_str());
+
+		self.scope.push(Scope::Fun);
+		let current = self.builder.get_insert_block();
+		self.builder.position_at_end(body_block);
+
+		// Set parameters in function body
+		for (i, parameter) in fun.get_param_iter().enumerate() {
+			let parameter_name = match &parameters[i].0.literal {
+				Some(Literal::String(str)) => str,
+				_ => {
+					return Err(Error::new_compiler_error(
+						"Invalid token/literal".to_string(),
+					))
+				}
+			};
+
+			parameter.set_name(parameter_name.as_str());
+
+			let alloca = self
+				.builder
+				.build_alloca(parameter.get_type(), &parameter_name);
+			self.builder.build_store(alloca, parameter);
+			self.variables.insert(parameter_name.to_string(), alloca);
+		}
+
+		self.evaluate_statement(*body)?;
+		self.scope.pop();
+		self.builder.position_at_end(current.unwrap());
+		
+		Ok(Operation::NoOp)
 	}
 
 	pub fn evaluate_continue(&mut self) -> Result<Operation, Error> {
@@ -226,6 +331,29 @@ impl Compiler<'_> {
 	pub fn evaluate_return(&self, right: Option<Expression>) -> Result<Operation, Error> {
 		use Operation::*;
 
+		let len = self.scope.len();
+		for index in 0..len {
+			if let Scope::Fun = self.scope[len - index - 1] {
+				if let Some(expression) = right {
+					match self.evaluate_expression(expression) {
+						Ok(FloatValue(result)) => self.builder.build_return(Some(&result)),
+						Ok(BoolValue(result)) => self.builder.build_return(Some(&result)),
+						Err(err) => return Err(err),
+						_ => {
+							return Err(Error::new_compiler_error(
+								"Unsupported return operation".to_string(),
+							))
+						}
+					};
+				} else {
+					self.builder.build_return(None);
+				}
+
+				return Ok(Operation::NoOp);
+			}
+		}
+
+		// Return on top-level program - should be removed when top-level program statement is added.
 		match right {
 			Some(expression) => match self.evaluate_expression(expression) {
 				Ok(FloatValue(result)) => Ok(Return(self.builder.build_return(Some(&result)))),
@@ -354,9 +482,9 @@ impl Compiler<'_> {
 	}
 
 	pub fn evaluate_expression(&self, expression: Expression) -> Result<Operation, Error> {
+		use self::Literal::*;
 		use Expression::*;
 		use Operation::*;
-		use self::Literal::*;
 
 		match expression {
 			Literal(token) => match token.literal {
@@ -440,17 +568,13 @@ impl Compiler<'_> {
 			Unary(_, expression) => {
 				let value = self.evaluate_expression(*expression)?;
 				match value {
-					BoolValue(value) => {
-						Ok(BoolValue(self.builder.build_not(value, "not")))
-					}
+					BoolValue(value) => Ok(BoolValue(self.builder.build_not(value, "not"))),
 					_ => Err(Error::new_compiler_error(
 						"Unsupported type for operation".to_string(),
 					)),
 				}
 			}
-			Binary(left, operator, right) => {
-				self.evaluate_operator(*left, *right, operator)
-			}
+			Binary(left, operator, right) => self.evaluate_operator(*left, *right, operator),
 			Grouping(expression) => self.evaluate_expression(*expression),
 			Call(function, _, parameters, _) => {
 				let name = match *function {
